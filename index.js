e56de851782f02eff8d290c7c263ede81cfb18c2
sangtik <sangtik@kakao.com>
2020-12-03 23:32:06 +0900

    post request success

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://sangtik:test@cluster0.8kskx.mongodb.net/sangtik?retryWrites=true&w=majority',{
@@ -10,6 +12,19 @@ mongoose.connect('mongodb+srv://sangtik:test@cluster0.8kskx.mongodb.net/sangtik?
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 clinet로 부터 받아 DB에 입력
+
+    // body 사용하려면 body-parser가 필요함
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
